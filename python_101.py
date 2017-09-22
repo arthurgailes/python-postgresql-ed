ab8bd7a03be71cc5965dc0fad5a6651fe8bbafcc
arthurgailes <agailes1@gmail.com>
2017-09-21 23:33:48 -0400

    starting datacamp

--- python_101.py
+++
@@ -1,8 +0,0 @@
-age = input('Enter your age: ')
-
-
-seconds = (int(age)*365*24*60*60)
-
-#using branckets inside print without needing to make it a string
-print("You have lived for {} seconds. This corresponds to {} years".format(seconds, age))
-
